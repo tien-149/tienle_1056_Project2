2d486f62b3cb6e0aee57585c4b50a9c21d94bb1b
Le Thi Cam Tien <thicamtien.le@users-iMac.localdomain>
2019-01-27 16:08:24 -0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//create a pifckel jar, label it as firstNam, put a string in it 
+var firstName = "Tien";
+
+//alert("Hello " + firstName);
+
+// modern js
+
+//alert(`Hello ${firstName} !!!`)
+
+//console.log(firstName);
+
+console.log("JavaScrip is linked up");
